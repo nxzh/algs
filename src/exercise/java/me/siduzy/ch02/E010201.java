5e5197f3d503e281eff38c564a5e8aeebcb859de
Sid Zhang <zhangnaixiao@outlook.com>
2020-06-09 01:12:21 +0900

    Add 1.2.1-1.2.12

--- src/exercise/java/me/siduzy/ch02/E010201.java
+++ src/exercise/java/me/siduzy/ch02/E010201.java
@@ -1,5 +1,6 @@
 package me.siduzy.ch02;
 
+import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
@@ -11,18 +12,16 @@ public class E010201 {
     public static final int CANVAS_SIZE = 2000;
     public static final int SCALE_SIZE = 100;
 
-    public static int[] p1;
-    public static int[] p2;
+    public static Point2D p1;
+    public static Point2D p2;
 
-    private static double distance(int[] p1, int[] p2) {
-        return Math.sqrt((Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2)));
-    }
 
-    public static double getShortest(int[][] points) {
+
+    public static double getShortest(Point2D[] points) {
         double minDist = Double.MAX_VALUE;
         for (int i = 0; i < points.length - 1; i++) {
             for (int j = i + 1; j < points.length; ++j) {
-                double dist = distance(points[i], points[j]);
+                double dist = points[i].distanceTo(points[j]);
                 if (dist < minDist) {
                     minDist = dist;
                     p1 = points[i];
@@ -35,10 +34,9 @@ public class E010201 {
 
     public static void main(String[] args) {
         int N = StdIn.readInt();
-        int[][] points = new int[N][2];
+        Point2D[] points = new Point2D[N];
         for (int i = 0; i < N; i++) {
-            points[i][0] = StdRandom.uniform(SCALE_SIZE);
-            points[i][1] = StdRandom.uniform(SCALE_SIZE);
+            points[i] = new Point2D(StdRandom.uniform(SCALE_SIZE), StdRandom.uniform(SCALE_SIZE));
         }
         StdDraw.setCanvasSize(CANVAS_SIZE, CANVAS_SIZE);
         StdDraw.setScale(0, SCALE_SIZE);
@@ -50,11 +48,11 @@ public class E010201 {
         StdDraw.setPenRadius(0.05);
         StdDraw.setPenColor(Color.BLUE);
         for (int i = 0; i < N; i++) {
-            StdDraw.point(points[i][0], points[i][1]);
+            StdDraw.point(points[i].x(), points[i].y());
         }
         System.out.println("Shortest distance is " + getShortest(points));
         StdDraw.setPenColor(Color.RED);
-        StdDraw.point(p1[0], p1[1]);
-        StdDraw.point(p2[0], p2[1]);
+        StdDraw.point(p1.x(), p1.y());
+        StdDraw.point(p2.x(), p2.y());
     }
 }
---
+++ src/exercise/java/me/siduzy/ch02/E010203.java
@@ -0,0 +1,8 @@
+package me.siduzy.ch02;
+
+public class E010203 {
+    //TODO: Add implementation
+    public static void main(String[] args) {
+
+    }
+}

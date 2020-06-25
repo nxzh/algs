e2329d9e2be9e8dace34d2292d72d9e263fecdab
Sid Zhang <zhangnaixiao@outlook.com>
2020-06-26 01:48:44 +0900

    update the queue implemented by array.

--- src/exercise/java/me/siduzy/ch01/sec03/E010314.java
+++ src/exercise/java/me/siduzy/ch01/sec03/E010314.java
@@ -1,5 +1,128 @@
 package me.siduzy.ch01.sec03;
 
+import edu.princeton.cs.algs4.StdOut;
+import me.siduzy.collection.Queue;
+
+import java.util.Iterator;
+
 public class E010314 {
-    
+    public static class ResizingArrayQueueOfStrings<Item> implements Queue<Item> {
+
+        private static final int DEFAULT_SIZE = 2;
+        private Item[] storage;
+        private int begin;
+        private int N;
+
+
+        public ResizingArrayQueueOfStrings() {
+            this(DEFAULT_SIZE);
+        }
+
+        public ResizingArrayQueueOfStrings(int capacity) {
+            storage = (Item[]) new Object[capacity];
+        }
+
+        @Override
+        public void enqueue(Item item) {
+            if (size() == storage.length) {
+                resize(storage.length * 2);
+            }
+            int end = (begin + N) % storage.length;
+            storage[end] = item;
+            ++N;
+        }
+
+        @Override
+        public Item dequeue() {
+            if (size() == 0) {
+                return null;
+            }
+            if (size() == storage.length / 4) {
+                resize(storage.length / 2);
+            }
+            Item ret = storage[begin];
+            storage[begin++] = null;
+            if (begin == storage.length) {
+                begin = begin % storage.length;
+            }
+            --N;
+            return ret;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return N == 0;
+        }
+
+        @Override
+        public int size() {
+            return N;
+        }
+
+        @Override
+        public Iterator<Item> iterator() {
+            return new FifoIterator(begin, N);
+        }
+
+        private void resize(int newSize) {
+            Item[] newStorage = (Item[]) new Object[newSize];
+            for (int i = 0; i < N; i++, begin++) {
+                if (begin == storage.length) {
+                    begin = 0;
+                }
+                newStorage[i] = storage[begin];
+            }
+            begin = 0;
+            storage = newStorage;
+
+        }
+
+        public class FifoIterator implements Iterator<Item> {
+            private int begin;
+            private int N;
+
+            public FifoIterator(int begin, int N) {
+                this.begin = begin;
+                this.N = N;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return N != 0;
+            }
+
+            @Override
+            public Item next() {
+                Item ret = storage[begin++];
+                if (begin == storage.length) {
+                    begin = 0;
+                }
+                --N;
+                return ret;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ResizingArrayQueueOfStrings<Integer> queue = new ResizingArrayQueueOfStrings<>();
+        queue.enqueue(0);
+        queue.enqueue(1); // en
+        queue.enqueue(2);
+        queue.enqueue(3);
+        StdOut.println(queue.size());
+        queue.dequeue();
+        queue.dequeue();
+        StdOut.println(queue.size());
+        queue.enqueue(0);
+        queue.enqueue(1);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+        StdOut.println(queue.size());
+
+        Iterator<Integer> iter = queue.iterator();
+        while (iter.hasNext()) {
+            StdOut.println(iter.next());
+        }
+    }
 }
--- src/main/java/me/siduzy/collection/ArrayQueue.java
+++ src/main/java/me/siduzy/collection/ArrayQueue.java
@@ -9,14 +9,19 @@ import java.util.NoSuchElementException;
  * @param <ITEM>
  */
 public class ArrayQueue<ITEM> implements Queue<ITEM> {
-    private static int DEFAULT_INITIAL_SIZE = 2;
+
+    private static final int DEFAULT_SIZE = 2;
     private ITEM[] storage;
-    private int head = 0;
-    private int tail = 0;
-    private int size = 0;
+    private int begin;
+    private int N;
+
 
     public ArrayQueue() {
-        storage = (ITEM[]) new Object[DEFAULT_INITIAL_SIZE];
+        this(DEFAULT_SIZE);
+    }
+
+    public ArrayQueue(int capacity) {
+        storage = (ITEM[]) new Object[capacity];
     }
 
     @Override
@@ -24,85 +29,77 @@ public class ArrayQueue<ITEM> implements Queue<ITEM> {
         if (size() == storage.length) {
             resize(storage.length * 2);
         }
-        if (tail == storage.length) {
-            tail = tail % storage.length;
-        }
-        storage[tail++] = item;
-        ++size;
-    }
-
-    private void resize(int newSize) {
-        ITEM[] newStorage = (ITEM[]) new Object[newSize];
-        int newTail = 0;
-        if (tail > head) {
-            for (int i = head; i < tail; i++, newTail++) {
-                newStorage[newTail] = storage[i];
-            }
-        } else {
-            for (int i = head; i < size(); i++, newTail++) {
-                newStorage[newTail] = storage[i];
-            }
-            for (int i = 0; i < tail; i++, newTail++) {
-                newStorage[newTail] = storage[i];
-            }
-        }
-        head = 0;
-        tail = newTail;
-        storage = newStorage;
+        int end = (begin + N) % storage.length;
+        storage[end] = item;
+        ++N;
     }
 
     @Override
     public ITEM dequeue() {
-        if (isEmpty()) {
+        if (size() == 0) {
             throw new NoSuchElementException();
         }
-        ITEM ret = storage[head];
-        storage[head++] = null;
-        --size;
-        if (head == storage.length) {
-            head = head % storage.length;
-        }
-        if (size() > 0 && size() == storage.length / 4) {
+        if (size() == storage.length / 4) {
             resize(storage.length / 2);
         }
+        ITEM ret = storage[begin];
+        storage[begin++] = null;
+        if (begin == storage.length) {
+            begin = begin % storage.length;
+        }
+        --N;
         return ret;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return N == 0;
     }
 
     @Override
     public int size() {
-        return size;
+        return N;
     }
 
     @Override
     public Iterator<ITEM> iterator() {
-        return new NaturalOrderIterator();
+        return new FifoIterator(begin, N);
     }
 
-    private class NaturalOrderIterator implements Iterator<ITEM> {
-        private int head;
-        private int tail;
+    private void resize(int newSize) {
+        ITEM[] newStorage = (ITEM[]) new Object[newSize];
+        for (int i = 0; i < N; i++, begin++) {
+            if (begin == storage.length) {
+                begin = 0;
+            }
+            newStorage[i] = storage[begin];
+        }
+        begin = 0;
+        storage = newStorage;
 
-        public NaturalOrderIterator() {
-            this.head = ArrayQueue.this.head;
-            this.tail = ArrayQueue.this.tail;
+    }
+
+    public class FifoIterator implements Iterator<ITEM> {
+        private int begin;
+        private int N;
+
+        public FifoIterator(int begin, int N) {
+            this.begin = begin;
+            this.N = N;
         }
 
         @Override
         public boolean hasNext() {
-            return head != tail;
+            return N != 0;
         }
 
         @Override
         public ITEM next() {
-            ITEM ret = storage[head++];
-            if (tail < head && head == storage.length) {
-                head = head % storage.length;
+            ITEM ret = storage[begin++];
+            if (begin == storage.length) {
+                begin = 0;
             }
+            --N;
             return ret;
         }
     }
